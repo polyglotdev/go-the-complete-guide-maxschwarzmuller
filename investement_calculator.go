a4049a7b146aae383460211cfb329920ebf21069
Dom Hallan <dom@domhallan.com>
2024-04-18 14:38:03 -0400

    Refactor investment_calculator.go to calculate future real value

--- investement_calculator.go
+++ investement_calculator.go
@@ -5,9 +5,15 @@ import (
 	"math"
 )
 
+const (
+	inflationRate = 2.5
+)
+
 func main() {
 	investmentAmount, expectedReturn, years := 1000.00, 5.5, 10.0
 
 	var futureValue = investmentAmount * math.Pow(1+expectedReturn/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 	fmt.Printf("Future value is: $%.2f\n", futureValue)
+	fmt.Printf("Future real value is: $%.2f\n", futureRealValue)
 }
