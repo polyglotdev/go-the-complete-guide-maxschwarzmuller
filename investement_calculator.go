47e955ce38763ed2498fa3b1c9c09b1305d076e3
Dom Hallan <dom@domhallan.com>
2024-04-18 14:26:52 -0400

    Refactor investment_calculator.go to use shorthand variable declaration

--- investement_calculator.go
+++ investement_calculator.go
@@ -6,9 +6,9 @@ import (
 )
 
 func main() {
-	var investmentAmount = 1000.00
-	var expectedReturn = 5.5
-	var years = 10.0
+	investmentAmount := 1000.00
+	expectedReturn := 5.5
+	years := 10.0
 
 	var futureValue = investmentAmount * math.Pow(1+expectedReturn/100, years)
 	fmt.Printf("Future value is: $%.2f\n", futureValue)
