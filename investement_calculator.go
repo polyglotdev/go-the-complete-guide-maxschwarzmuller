f0b16948d1be90a251be1cb79272f61381641c11
Dom Hallan <dom@domhallan.com>
2024-04-18 09:53:09 -0400

    add calculations for investment calclator

--- investement_calculator.go
+++ investement_calculator.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	var investmentAmount = 1000
+	var expectedReturn = 5.5
+	var years = 3
+
+	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturn/100, float64(years))
+	fmt.Println("Future value is: $", futureValue)
 }
