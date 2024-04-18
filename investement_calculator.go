e2ac2432e3ce395e86afc6dac31b96bf370b7ae8
Dom Hallan <dom@domhallan.com>
2024-04-18 13:55:42 -0400

    change type on investmentAmount

--- investement_calculator.go
+++ investement_calculator.go
@@ -6,10 +6,10 @@ import (
 )
 
 func main() {
-	var investmentAmount = 1000
+	var investmentAmount float64 = 1000.00
 	var expectedReturn = 5.5
 	var years = 10
 
-	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturn/100, float64(years))
+	var futureValue = investmentAmount * math.Pow(1+expectedReturn/100, float64(years))
 	fmt.Println("Future value is: $", futureValue)
 }
