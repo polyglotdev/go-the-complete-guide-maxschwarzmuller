03eb35994dc9a730a5e4a7ed69ed4cd4d8145c71
Dom Hallan <dom@domhallan.com>
2024-04-18 13:54:17 -0400

    change value of years

--- investement_calculator.go
+++ investement_calculator.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	var investmentAmount = 1000
 	var expectedReturn = 5.5
-	var years = 3
+	var years = 10
 
 	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturn/100, float64(years))
 	fmt.Println("Future value is: $", futureValue)
