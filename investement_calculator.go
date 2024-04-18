bdca95646f499717e634b860e0a2cfa899fa3e11
Dom Hallan <dom@domhallan.com>
2024-04-18 13:58:06 -0400

    Refactor investment_calculator.go to use float64 for years variable

--- investement_calculator.go
+++ investement_calculator.go
@@ -6,10 +6,10 @@ import (
 )
 
 func main() {
-	var investmentAmount float64 = 1000.00
+	var investmentAmount = 1000.00
 	var expectedReturn = 5.5
-	var years = 10
+	var years = 10.0
 
-	var futureValue = investmentAmount * math.Pow(1+expectedReturn/100, float64(years))
+	var futureValue = investmentAmount * math.Pow(1+expectedReturn/100, years)
 	fmt.Println("Future value is: $", futureValue)
 }
