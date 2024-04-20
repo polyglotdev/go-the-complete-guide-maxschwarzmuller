b7d2314891b7acb775c347e42042bafba0638cec
Dom Hallan <dom@domhallan.com>
2024-04-20 17:51:42 -0400

    Refactor investment_calculator.go to use getUserInput function for user input and calculate future value and future real value

--- investement_calculator.go
+++ investement_calculator.go
@@ -10,22 +10,50 @@ const (
 )
 
 func main() {
-	var investmentAmount float64
-	var expectedReturn float64
-	var years float64
+	investmentAmount, expectedReturn, years := getUserInput()
 
-	fmt.Print("Investment amount: ")
-	fmt.Scan(&investmentAmount)
-
-	fmt.Print("Expected Return Rate: ")
-	fmt.Scan(&expectedReturn)
-
-	fmt.Print("Years: ")
-	fmt.Scan(&years)
-
-	futureValue := investmentAmount * math.Pow(1+expectedReturn/100, years)
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	futureValue := calculateFutureValue(investmentAmount, expectedReturn, years)
+	futureRealValue := calculateFutureRealValue(futureValue, years)
 
 	fmt.Printf("Future value is: $%.2f\n", futureValue)
 	fmt.Printf("Future real value is: $%.2f\n", futureRealValue)
+}
+
+// getUserInput is a function that prompts the user to input their investment details.
+// It returns three float64 values: the investment amount, the expected return rate, and the number of years.
+func getUserInput() (investmentAmount, expectedReturn, years float64) {
+	// Prompt the user for the investment amount
+	fmt.Print("Investment amount: ")
+	_, err := fmt.Scan(&investmentAmount)
+	// If there's an error reading the input, print an error message
+	if err != nil {
+		fmt.Println("Error reading investment amount:", err)
+	}
+
+	// Prompt the user for the expected return rate
+	fmt.Print("Expected Return Rate: ")
+	_, err = fmt.Scan(&expectedReturn)
+	// If there's an error reading the input, print an error message
+	if err != nil {
+		fmt.Println("Error reading expected return rate:", err)
+	}
+
+	// Prompt the user for the number of years
+	fmt.Print("Years: ")
+	_, err = fmt.Scan(&years)
+	// If there's an error reading the input, print an error message
+	if err != nil {
+		fmt.Println("Error reading years:", err)
+	}
+
+	// Return the investment details
+	return
+}
+
+func calculateFutureValue(investmentAmount, expectedReturn, years float64) float64 {
+	return investmentAmount * math.Pow(1+expectedReturn/100, years)
+}
+
+func calculateFutureRealValue(futureValue, years float64) float64 {
+	return futureValue / math.Pow(1+inflationRate/100, years)
 }
