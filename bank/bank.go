13e711e254677adc120e00a8625f9087af39c569
Dom Hallan <dom@domhallan.com>
2024-04-20 18:35:15 -0400

    Refactor bank.go to improve user interface and menu options, and update go version in go.mod to 1.22.2

--- bank/bank.go
+++ bank/bank.go
@@ -1,43 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Welcome to Go Bank!")
-	fmt.Println("What do you want to do?")
-	fmt.Println("1. Check Balance")
-	fmt.Println("2. Deposit money")
-	fmt.Println("3. Withdraw money")
-	fmt.Println("4. Exit")
-
-	// user input
-	var choice int
-	fmt.Print("Your choice: ")
-	fmt.Scanln(&choice)
 	balance := 1000.00
+	var choice int
 	var depositedAmount float64
 
-	switch choice {
-	case 1:
-		fmt.Printf("Your balance is: $%.2f\n", balance)
-	case 2:
-		fmt.Print("Enter amount to deposit: ")
-		fmt.Scanln(&depositedAmount)
-		balance += depositedAmount
-		fmt.Printf("Your balance is: $%.2f\n", balance)
-	case 3:
-		fmt.Println("Withdraw money")
-		fmt.Print("Enter amount to withdraw: ")
-		fmt.Scanln(&depositedAmount)
-		if depositedAmount > balance {
-			fmt.Println("Insufficient balance")
-		} else {
-			balance -= depositedAmount
+	for {
+		fmt.Println("Welcome to Go Bank!")
+		fmt.Println("What do you want to do?")
+		fmt.Println("1. Check Balance")
+		fmt.Println("2. Deposit money")
+		fmt.Println("3. Withdraw money")
+		fmt.Println("4. Exit")
+
+		fmt.Print("Your choice: ")
+		fmt.Scanln(&choice)
+
+		switch choice {
+		case 1:
 			fmt.Printf("Your balance is: $%.2f\n", balance)
+		case 2:
+			fmt.Print("Enter amount to deposit: ")
+			fmt.Scanln(&depositedAmount)
+			balance += depositedAmount
+			fmt.Printf("Your balance is: $%.2f\n", balance)
+		case 3:
+			fmt.Print("Enter amount to withdraw: ")
+			fmt.Scanln(&depositedAmount)
+			if depositedAmount > balance {
+				fmt.Println("Insufficient balance")
+			} else {
+				balance -= depositedAmount
+				fmt.Printf("Your balance is: $%.2f\n", balance)
+			}
+		case 4:
+			fmt.Println("Exit")
+			os.Exit(0)
+		default:
+			fmt.Println("Invalid choice")
 		}
-	case 4:
-		fmt.Println("Exit")
-	default:
-		fmt.Println("Invalid choice")
 	}
 }
