ccf29077c752d1c2f91b0872582a3d775929af6d
Dom Hallan <dom@domhallan.com>
2024-04-21 14:48:44 -0400

    Refactor main function in fndeepdive package to use range loop for modifying slice elements

--- fndeepdive/fndeepdive.go
+++ fndeepdive/fndeepdive.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	nums := []int{1, 2, 3, 4, 5}
+	fmt.Println(nums)
+	for i, value := range nums {
+		nums[i] = value * 2
+	}
+	fmt.Println(nums)
+	fmt.Println("------------------------------")
+	otherNums := []int{2, 4, 6, 8, 10}
+	fmt.Println(otherNums)
+	for index := 0; index < len(otherNums); index++ {
+		otherNums[index] = otherNums[index] * 10
+	}
+	fmt.Println(otherNums)
 }
