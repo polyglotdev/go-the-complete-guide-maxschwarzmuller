9023ed7c1398ab084a6fdc44570f621ebb31685a
Dom Hallan <dom@domhallan.com>
2024-04-21 16:53:55 -0400

    Refactor main function in calculator.go to remove unused code and improve readability

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -1,19 +1,14 @@
 package main
 
 import (
-	"fmt"
-
 	"example.com/calculator/prices"
 )
 
 func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
-	result := make(map[float64][]float64)
-
 	for _, taxRate := range taxRates {
 		priceJob := prices.NewPriceJobWithTax(taxRate)
 		priceJob.Process()
 	}
-	fmt.Printf("Result: %v\n", result)
 }
