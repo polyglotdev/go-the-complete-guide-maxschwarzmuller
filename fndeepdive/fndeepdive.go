9f94ebaabfe415c5fdfc96e4587b5ed91ae62e43
Dom Hallan <dom@domhallan.com>
2024-04-21 14:57:28 -0400

    Refactor main function in fndeepdive package to use range loop for doubling numbers

--- fndeepdive/fndeepdive.go
+++ fndeepdive/fndeepdive.go
@@ -1,4 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
+	numbers := []int{1, 2, 3, 4, 5}
+	result := doubleNumbers(numbers)
+	fmt.Println(result)
+}
+
+func doubleNumbers(n []int) []int {
+	result := make([]int, len(n))
+	for i, v := range n {
+		result[i] = v * 2
+	}
+	return result
 }
