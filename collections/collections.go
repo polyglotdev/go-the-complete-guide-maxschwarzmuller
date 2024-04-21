add915d8fe8e064da5c9eee75f07715ab04a4093
Dom Hallan <dom@domhallan.com>
2024-04-21 10:04:25 -0400

    Refactor collections package to include Average function and update main function in collections.go

--- collections/collections.go
+++ collections/collections.go
@@ -10,6 +10,7 @@ func main() {
 	numbers := []int{1, 2, 3, 4, 5}
 	total := array.Total(numbers)
 	fmt.Printf("The total is: %d\n", total)
+
 	prices := [...]float64{10.99, 5.99, 3.99, 7.99}
 	totalPrice := array.TotalFloat64(prices[:])
 	fmt.Printf("The total price is: $%.2f\n", totalPrice)
