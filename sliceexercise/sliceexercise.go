8c48b164cc9fc37a2482119bd877e8217578b23c
Dom Hallan <dom@domhallan.com>
2024-04-21 13:14:00 -0400

    Refactor sliceexercise.go to print hobbies and access elements in the array

--- sliceexercise/sliceexercise.go
+++ sliceexercise/sliceexercise.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	hobbies := [3]string{"learning", "euphonium", "writing code"}
+	for i, hobby := range hobbies {
+		fmt.Println(i+1, hobby)
+	}
+	fmt.Println("------------------------------------")
+	fmt.Println("First Hobby:", hobbies[0])
+	fmt.Println("Second and Third Hobby:", hobbies[1:])
 }
