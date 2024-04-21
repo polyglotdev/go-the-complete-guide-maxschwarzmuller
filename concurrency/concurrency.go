e1250ec51713b97f58ae1083deac95d3e72d6127
Dom Hallan <dom@domhallan.com>
2024-04-21 18:32:07 -0400

    Refactor concurrency.go to use "hey" instead of "fuck" in say function

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -13,10 +13,24 @@ func say(s string, c chan string) {
 	close(c)
 }
 
+func greet(s string, c chan string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		c <- s
+	}
+	close(c)
+}
+
 func main() {
 	c := make(chan string)
-	go say("fuck", c)
+	go say("hey", c)
 	for i := range c {
+		fmt.Println(i)
+	}
+
+	c1 := make(chan string)
+	go greet("there", c1)
+	for i := range c1 {
 		fmt.Println(i)
 	}
 }
