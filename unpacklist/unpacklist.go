396cb467cb06680487513d35da4a1352b6f869da
Dom Hallan <dom@domhallan.com>
2024-04-21 13:40:58 -0400

    Refactor unpacklist.go to add print statements and re-slice prices slice

--- unpacklist/unpacklist.go
+++ unpacklist/unpacklist.go
@@ -4,6 +4,11 @@ import "fmt"
 
 func main() {
 	prices := []float64{100.45, 10046.89, 850000.20, 67643.83}
+	fmt.Printf("Prices Header: %T\nCapacity: %d\nLength: %d\nPrices: %v\n", prices, cap(prices), len(prices), prices)
+	fmt.Println("----------------------------------------------------------------------")
 	prices = append(prices, 3458.98, 576.23)
-	fmt.Println(prices)
+	fmt.Println("Prices:", prices)
+
+	prices = prices[1:]
+	fmt.Println("Prices after re-slice:", prices)
 }
