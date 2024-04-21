9a6b5c3ba2fd7c650b2e7eb733bc9e91a65cab6b
Dom Hallan <dom@domhallan.com>
2024-04-21 10:53:35 -0400

    Add code to append the number six to the end of threeFourFive in collections.go

--- collections/collections.go
+++ collections/collections.go
@@ -32,4 +32,29 @@ func main() {
 	myArray := [5]int{1, 2, 3, 4, 5}
 	threeFourFive := myArray[2:]
 	fmt.Printf("The last three elements in myArray are: %v\n", threeFourFive)
+
+	// add the number six to the end of threeFourFive
+	threeFourFive = append(threeFourFive, 6)
+	fmt.Printf("The last three elements in myArray are: %v\n", threeFourFive)
+
+	slice1 := []int{1, 2, 3}
+	slice2 := slice1
+
+	fmt.Println("------------------------------------")
+	fmt.Printf("slice1 memory address: %p\n", &slice1)
+	fmt.Printf("slice2 memory address: %p\n", &slice2)
+
+	fmt.Println("------------------------------------")
+	array1 := [3]int{1, 2, 3}
+	array2 := array1
+
+	fmt.Printf("array1 memory address: %p\n", &array1)
+	fmt.Printf("array2 memory address: %p\n", &array2)
+	fmt.Println("------------------------------------")
+
+	// slice1 and slice2 point to the same memory address
+	// so when we change slice1, slice2 will also change
+	slice1[0] = 4
+	fmt.Printf("slice1: %v\n", slice1)
+	fmt.Printf("slice2: %v\n", slice2)
 }
