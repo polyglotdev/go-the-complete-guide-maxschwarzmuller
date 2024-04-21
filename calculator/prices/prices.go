00d28dcebab3b35dc61e6ae6d8d5cf0219fa5bd5
Dom Hallan <dom@domhallan.com>
2024-04-21 16:32:45 -0400

    Refactor Calculate method in PriceJobWithTax struct to apply tax rates to input prices

--- calculator/prices/prices.go
+++ calculator/prices/prices.go
@@ -1,14 +1,22 @@
 package prices
 
+import "fmt"
+
 type PriceJobWithTax struct {
 	TaxRate           float64
 	InputPrices       []float64
 	TaxIncludedPrices map[string]float64
 }
 
-func NewPriceJobWithTax(taxRate float64, inputPrices []float64) *PriceJobWithTax {
+func (p *PriceJobWithTax) Calculate() {
+	for _, price := range p.InputPrices {
+		p.TaxIncludedPrices[fmt.Sprintf("%.2f", price)] = price * (1 + p.TaxRate)
+	}
+}
+
+func NewPriceJobWithTax(taxRate float64) *PriceJobWithTax {
 	return &PriceJobWithTax{
-		InputPrices:       inputPrices,
+		InputPrices:       []float64{10, 20, 30},
 		TaxRate:           taxRate,
 		TaxIncludedPrices: make(map[string]float64),
 	}
