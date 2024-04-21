c0183efc64d4f3bc8fb83ca3b4d6a141d777fb7b
Dom Hallan <dom@domhallan.com>
2024-04-21 10:04:52 -0400

    Add AverageFloat64 function to calculate the average of a slice of float64s

--- collections/array/array.go
+++ collections/array/array.go
@@ -23,3 +23,9 @@ func Average(numbers []int) float64 {
 	total := Total(numbers)
 	return float64(total) / float64(len(numbers))
 }
+
+// AverageFloat64 takes in a slice of float64s and returns the average of all the float64s in the slice.
+func AverageFloat64(prices []float64) float64 {
+	total := TotalFloat64(prices)
+	return total / float64(len(prices))
+}
