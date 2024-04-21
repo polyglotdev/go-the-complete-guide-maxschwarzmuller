8ce149232e44f75827370230f7c5ff2a4498ad32
Dom Hallan <dom@domhallan.com>
2024-04-21 17:13:14 -0400

    Refactor LoadData method in PriceJobWithTax struct to read prices from prices.txt file

--- calculator/prices/prices.go
+++ calculator/prices/prices.go
@@ -1,6 +1,11 @@
 package prices
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 // PriceJobWithTax represents a job that processes prices with tax.
 type PriceJobWithTax struct {
@@ -13,11 +18,53 @@ type PriceJobWithTax struct {
 // It creates a map where the keys are the original prices (as strings) and the values are the prices after tax.
 // The results are not returned, but can be accessed through the TaxRate field of the PriceJobWithTax struct.
 func (p *PriceJobWithTax) Process() {
-	result := make(map[string]float64)
+	p.LoadData()
+
+	result := make(map[string]string)
 	for _, price := range p.InputPrices {
-		result[fmt.Sprintf("%.2f", price)] = price * (1 + p.TaxRate)
+		taxIncludedPrices := price * (1 + p.TaxRate)
+		result[fmt.Sprintf("%.2f", price)] = fmt.Sprintf("%.2f", taxIncludedPrices)
 	}
 	fmt.Println(result)
+}
+
+func (j *PriceJobWithTax) LoadData() {
+	file, err := os.Open("prices.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	var lines []string
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	err = scanner.Err()
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		file.Close()
+		return
+	}
+
+	prices := make([]float64, len(lines))
+
+	for lineIndex, line := range lines {
+		floatPrice, err := strconv.ParseFloat(line, 64)
+
+		if err != nil {
+			fmt.Println("Error parsing line", lineIndex, ":", err)
+			file.Close()
+			return
+		}
+
+		prices[lineIndex] = floatPrice
+	}
+
+	j.InputPrices = prices
 }
 
 // NewPriceJobWithTax creates a new PriceJobWithTax object.
