d000950b10c3c54d6878092c584f292206f5249c
Dom Hallan <dom@domhallan.com>
2024-04-21 12:31:43 -0400

    Add unit test for TotalFloat64 function in array_test.go

--- collections/array/array_test.go
+++ collections/array/array_test.go
@@ -29,3 +29,27 @@ func TestTotal(t *testing.T) {
 		ExpectedInt: 15,
 	})
 }
+
+func TestTotalFloat64(t *testing.T) {
+	type testCase struct {
+		Name string
+
+		Prices []float64
+
+		ExpectedFloat64 float64
+	}
+
+	validate := func(t *testing.T, tc *testCase) {
+		t.Run(tc.Name, func(t *testing.T) {
+			actualFloat64 := TotalFloat64(tc.Prices)
+
+			assert.Equal(t, tc.ExpectedFloat64, actualFloat64)
+		})
+	}
+
+	validate(t, &testCase{
+		Name:            "TestTotalFloat64 with positive numbers",
+		Prices:          []float64{10.99, 5.99, 3.99, 7.99},
+		ExpectedFloat64: 28.96,
+	})
+}
