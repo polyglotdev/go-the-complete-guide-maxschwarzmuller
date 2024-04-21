29b528b94d09508e56eca41d62e1a9dba5af6f61
Dom Hallan <dom@domhallan.com>
2024-04-21 17:50:07 -0400

    Refactor WriteJSON function to write data to a file in filemanager.go

--- calculator/filemanager/filemanager.go
+++ calculator/filemanager/filemanager.go
@@ -2,6 +2,7 @@ package filemanager
 
 import (
 	"bufio"
+	"encoding/json"
 	"errors"
 	"os"
 )
@@ -28,4 +29,25 @@ func ReadLines(path string) ([]string, error) {
 
 	file.Close()
 	return lines, nil
+}
+
+// WriteJSON writes the data to a file at the specified path.
+// If the file does not exist, it will be created.
+// If the file already exists, it will be overwritten.
+// If an error occurs, it will be returned.
+func WriteJSON(path string, data []byte) error {
+	file, err := os.Create(path)
+	if err != nil {
+		return errors.New("error creating file: " + err.Error())
+	}
+
+	jsonEncoder := json.NewEncoder(file)
+	err = jsonEncoder.Encode(data)
+	if err != nil {
+		file.Close()
+		return errors.New("error encoding data: " + err.Error())
+	}
+
+	file.Close()
+	return nil
 }
