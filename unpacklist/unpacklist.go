69121f38b01b54126e3d254c4613e3ecf98c9ad9
Dom Hallan <dom@domhallan.com>
2024-04-21 13:37:55 -0400

    Add prices slice and append new prices in main function

--- unpacklist/unpacklist.go
+++ unpacklist/unpacklist.go
@@ -1,5 +1,9 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	prices := []float64{100.45, 10046.89, 850000.20, 67643.83}
+	prices = append(prices, 3458.98, 576.23)
+	fmt.Println(prices)
 }
