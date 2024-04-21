e55d9d57165a72de8fbdb00597fa9c6d9f76464a
Dom Hallan <dom@domhallan.com>
2024-04-21 19:24:36 -0400

    Refactor main.go to use goroutines for processing price jobs with different tax rates and handle errors in goroutines

--- concurrency-project/main.go
+++ concurrency-project/main.go
@@ -10,15 +10,24 @@ import (
 func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 	c := make([]chan bool, len(taxRates))
+	ec := make([]chan error, len(taxRates))
 
 	for i, taxRate := range taxRates {
 		c[i] = make(chan bool)
+		ec[i] = make(chan error)
 		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
 		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
-		go priceJob.Process(c[i])
+		go priceJob.Process(c[i], ec[i])
 	}
 
-	for _, ch := range c {
-		<-ch
-	}
+	go func() {
+		for i := range taxRates {
+			select {
+			case <-c[i]:
+				fmt.Println("Job completed successfully")
+			case err := <-ec[i]:
+				fmt.Println("Job failed with error:", err)
+			}
+		}
+	}()
 }
