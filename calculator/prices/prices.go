7fdf415dc8dee65e70208f01a0262f528b612471
Dom Hallan <dom@domhallan.com>
2024-04-21 16:29:35 -0400

    Refactor NewPriceJobWithTax function in prices module to initialize TaxRate, InputPrices, and TaxIncludedPrices

--- calculator/prices/prices.go
+++ calculator/prices/prices.go
@@ -5,3 +5,11 @@ type PriceJobWithTax struct {
 	InputPrices       []float64
 	TaxIncludedPrices map[string]float64
 }
+
+func NewPriceJobWithTax(taxRate float64, inputPrices []float64) *PriceJobWithTax {
+	return &PriceJobWithTax{
+		TaxRate:           taxRate,
+		InputPrices:       inputPrices,
+		TaxIncludedPrices: make(map[string]float64),
+	}
+}
