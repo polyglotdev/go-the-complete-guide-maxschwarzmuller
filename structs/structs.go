0a1b779696f9c9c120e3e583c915f5f340894e35
Dom Hallan <dom@domhallan.com>
2024-04-20 21:50:59 -0400

    Refactor createUser function in structs/structs.go to use user.New() and add admin user creation

--- structs/structs.go
+++ structs/structs.go
@@ -7,15 +7,19 @@ import (
 )
 
 func main() {
-	elijah, err := user.CreateUser()
+	elijah, err := user.New()
 	if err != nil {
 		log.Fatal(err)
 	}
 	elijah.PrintUser()
 
-	ezra, err := user.CreateUser()
+	ezra, err := user.New()
 	if err != nil {
 		log.Fatal(err)
 	}
 	ezra.PrintUser()
+
+	admin := user.NewAdmin("admin@admin.com", "admin", "Admin", "Admin", "01/01/2000")
+
+	admin.PrintUser()
 }
