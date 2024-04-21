116fb683649341efc62fb8417f277bef3a9e5169
Dom Hallan <dom@domhallan.com>
2024-04-21 15:05:34 -0400

    Refactor main function in fndeepdive package to use transformNumbers function for modifying slice elements

--- fndeepdive/fndeepdive.go
+++ fndeepdive/fndeepdive.go
@@ -4,14 +4,29 @@ import "fmt"
 
 func main() {
 	numbers := []int{1, 2, 3, 4, 5}
-	result := doubleNumbers(numbers)
-	fmt.Println(result)
+	result1 := transformNumbers(numbers, double)
+	result2 := transformNumbers(numbers, func(n int) int {
+		return n * 2
+	})
+	result3 := transformNumbers(numbers, func(n int) int {
+		return n * 3
+	})
+	fmt.Println("----------------------------------------")
+	fmt.Printf("Double numbers: %v\n", result1)
+	fmt.Printf("Double numbers as func: %v\n", result2)
+	fmt.Printf("Triple numbers: %v\n", result3)
+	fmt.Println("----------------------------------------")
+	fmt.Println("Numbers: ", numbers)
 }
 
-func doubleNumbers(n []int) []int {
+func double(number int) int {
+	return number * 2
+}
+
+func transformNumbers(n []int, f func(int) int) []int {
 	result := make([]int, len(n))
 	for i, v := range n {
-		result[i] = v * 2
+		result[i] = f(v)
 	}
 	return result
 }
