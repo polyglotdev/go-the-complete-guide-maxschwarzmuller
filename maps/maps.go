9376b34100b1e631071c8cffc675a68902b4785e
Dom Hallan <dom@domhallan.com>
2024-04-21 13:58:39 -0400

    Refactor main function in maps package to print cloud providers

--- maps/maps.go
+++ maps/maps.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	cloudProviders := map[string]string{
+		"aws":     "Amazon Web Services",
+		"gcp":     "Google Cloud Platform",
+		"az":      "Microsoft Azure",
+		"ibm":     "IBM Cloud",
+		"alibaba": "Alibaba Cloud",
+	}
+	for key, value := range cloudProviders {
+		fmt.Printf("%s:, %s\n", key, value)
+	}
 }
