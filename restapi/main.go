157d7d792e21e1f803110abaef555df024271ad8
Dom Hallan <dom@domhallan.com>
2024-04-21 20:09:58 -0400

    Add REST API endpoint for creating events in main.go

--- restapi/main.go
+++ restapi/main.go
@@ -13,6 +13,7 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	err := server.Run(":8080")
 	if err != nil {
@@ -24,4 +25,21 @@ func main() {
 func getEvents(c *gin.Context) {
 	events := models.GetEvents()
 	c.JSON(http.StatusOK, events)
+}
+
+func createEvent(c *gin.Context) {
+	var event models.Event
+	err := c.BindJSON(&event)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	err = event.Save()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, event)
 }
