4241a3b96c7ddee09328f2378e3e4563eda0881c
Dom Hallan <dom@domhallan.com>
2024-04-22 15:12:19 -0400

    Refactor REST API endpoints in main.go

--- restapi/main.go
+++ restapi/main.go
@@ -2,78 +2,24 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 
 	"example.com/rest-api/db"
-	"example.com/rest-api/models"
+	"example.com/rest-api/routes"
 )
 
 func main() {
 	db.InitDB()
 	server := gin.Default()
 
-	server.GET("/events", getEvents)
-	server.POST("/events/:id", createEvent)
-	server.POST("/events", createEvent)
+	server.GET("/events", routes.GetEvents)
+	server.GET("/events/:id", routes.GetEvent)
+	server.POST("/events", routes.CreateEvent)
 
 	err := server.Run(":8080")
 	if err != nil {
 		errMsg := fmt.Errorf("error starting server: %v", err)
 		fmt.Println(errMsg)
 	}
-}
-
-func getEvents(c *gin.Context) {
-	events, err := models.GetAllEvents()
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-	c.JSON(http.StatusOK, events)
-}
-
-func createEvent(c *gin.Context) {
-	var event models.Event
-	err := c.ShouldBindJSON(&event)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	event.ID = 1
-	event.UserID = 1
-
-	err = event.Save()
-
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-
-	c.JSON(http.StatusCreated, gin.H{"message": "Event created successfully", "event": event})
-}
-
-func getEvent(c *gin.Context) {
-	// Get the event ID from the URL
-	id := c.Param("id")
-
-	// Convert the string ID to an integer
-	eventID, err := strconv.Atoi(id)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid event ID"})
-		return
-	}
-
-	// Get the event from the database
-	event, err := models.GetEvent(eventID)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-
-	// Return the event as JSON
-	c.JSON(http.StatusOK, event)
 }
