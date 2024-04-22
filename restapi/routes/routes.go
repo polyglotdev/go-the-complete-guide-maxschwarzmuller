6a98f27d55aac94422fa28449d9b1778f08a5c50
Dom Hallan <dom@domhallan.com>
2024-04-22 15:12:45 -0400

    Add REST API endpoints for creating and retrieving events

---
+++ restapi/routes/routes.go
@@ -0,0 +1,11 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+// Register defines the routes for the REST API.
+// it takes a pointer to a gin.Engine instance as a parameter.
+func Register(server *gin.Engine) {
+	server.GET("/events", GetEvents)
+	server.GET("/events/:id", GetEvent)
+	server.POST("/events", CreateEvent)
+}
