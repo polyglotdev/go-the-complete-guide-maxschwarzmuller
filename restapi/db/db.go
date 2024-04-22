1dee83b999fce6fd1f05155395edb23643f680c3
Dom Hallan <dom@domhallan.com>
2024-04-22 17:20:17 -0400

    Add SQLite database initialization and table creation for users and events

--- restapi/db/db.go
+++ restapi/db/db.go
@@ -1,3 +1,5 @@
+// The code initializes a SQLite database connection and creates tables for users and events with
+// foreign key constraints.
 package db
 
 import (
@@ -40,7 +42,7 @@ func createTables() {
             description TEXT NOT NULL,
             location TEXT NOT NULL,
             dateTime DATETIME NOT NULL,
-            user_id INTEGER
+            user_id INTEGER,
 						FOREIGN KEY (user_id) REFERENCES users(id)
         );
         `
