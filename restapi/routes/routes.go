44e7e982592f6238aff88c19433c1bc9530225a4
Dom Hallan <dom@domhallan.com>
2024-04-22 16:38:14 -0400

    Add DeleteEvent REST API endpoint for deleting an event by ID

--- restapi/routes/routes.go
+++ restapi/routes/routes.go
@@ -9,4 +9,5 @@ func Register(server *gin.Engine) {
 	server.GET("/events/:id", GetEvent)
 	server.POST("/events", CreateEvent)
 	server.PUT("/events/:id", UpdateEvent)
+	server.DELETE("/events/:id", DeleteEvent)
 }
