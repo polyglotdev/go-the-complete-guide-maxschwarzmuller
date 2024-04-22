21b2cb587dc39eb671d79c397091a272b83d6add
Dom Hallan <dom@domhallan.com>
2024-04-21 20:50:31 -0400

    Refactor database initialization and createTables function in db.go

--- restapi/db/db.go
+++ restapi/db/db.go
@@ -16,4 +16,23 @@ func InitDB() {
 
 	DB.SetMaxOpenConns(10)
 	DB.SetMaxIdleConns(5)
+
+	createTables()
+}
+
+func createTables() {
+	createEventTable := `
+		CREATE TABLE IF NOT EXISTS events (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			title TEXT NOT NULL,
+			description TEXT NOT NULL,
+			location TEXT NOT NULL,
+			date DATETIME NOT NULL,
+			user_id INTEGER
+		);
+		`
+	_, err := DB.Exec(createEventTable)
+	if err != nil {
+		panic(err)
+	}
 }
