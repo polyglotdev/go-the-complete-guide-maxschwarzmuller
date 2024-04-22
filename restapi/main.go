9df837b2b015cdc846bc48386593f248ad8bd017
Dom Hallan <dom@domhallan.com>
2024-04-21 20:00:24 -0400

    Refactor main.go to add REST API endpoint for retrieving events

--- restapi/main.go
+++ restapi/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	server := gin.Default()
+
+	server.GET("/events", getEvents)
+
+	err := server.Run(":8080")
+	if err != nil {
+		errMsg := fmt.Errorf("error starting server: %v", err)
+		fmt.Println(errMsg)
+	}
+}
+
+func getEvents(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "GET events",
+	})
 }
