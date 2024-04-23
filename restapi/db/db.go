9184dc58e7db1a240c26710cc11b32ddc8c6d8d2
Dom Hallan <dom@domhallan.com>
2024-04-22 20:55:36 -0400

    Add registration table to the database schema

--- restapi/db/db.go
+++ restapi/db/db.go
@@ -50,4 +50,19 @@ func createTables() {
 	if err != nil {
 		panic(err)
 	}
+
+	createRegistrationTable := `
+				CREATE TABLE IF NOT EXISTS registrations (
+					id INTEGER PRIMARY KEY AUTOINCREMENT,
+					event_id INTEGER,
+					user_id INTEGER,
+					FOREIGN KEY (event_id) REFERENCES events(id),
+					FOREIGN KEY (user_id) REFERENCES users(id)
+				);
+				`
+
+	_, err = DB.Exec(createRegistrationTable)
+	if err != nil {
+		panic(err)
+	}
 }
