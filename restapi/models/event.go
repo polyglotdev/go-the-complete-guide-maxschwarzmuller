23704802c66f8edb8b0ab4d6b755ce8471adf226
Dom Hallan <dom@domhallan.com>
2024-04-22 21:13:19 -0400

    Add CancelRegistration functionality to Event model

--- restapi/models/event.go
+++ restapi/models/event.go
@@ -107,3 +107,16 @@ func (e Event) Register(userID int64) error {
 	_, err = stmt.Exec(e.ID, userID)
 	return err
 }
+
+// CancelRegistration cancels a user's registration for an event.
+func (e Event) CancelRegistration(userID int64) error {
+	query := `DELETE FROM registrations WHERE event_id = ? AND user_id = ?`
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(e.ID, userID)
+	return err
+}
