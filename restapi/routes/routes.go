4981f690cf6dc3aeb843f0c0659e168d31be62ef
Dom Hallan <dom@domhallan.com>
2024-04-22 20:19:11 -0400

    Refactor token authentication logic in CreateEvent endpoint and update UserID type to int64 in Event struct

--- restapi/routes/routes.go
+++ restapi/routes/routes.go
@@ -1,13 +1,17 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+
+	"example.com/rest-api/middlewares"
+)
 
 // Register defines the routes for the REST API.
 // it takes a pointer to a gin.Engine instance as a parameter.
 func Register(server *gin.Engine) {
 	server.GET("/events", GetEvents)
 	server.GET("/events/:id", GetEvent)
-	server.POST("/events", CreateEvent)
+	server.POST("/events", middlewares.CheckAuth, CreateEvent)
 	server.PUT("/events/:id", UpdateEvent)
 	server.DELETE("/events/:id", DeleteEvent)
 	server.POST("/signup", Signup)
