e7284ef2299aa435649cd0926a6f4dffeedbd388
Dom Hallan <dom@domhallan.com>
2024-04-22 21:13:28 -0400

    Add CancelRegistration functionality to RegisterForEvent endpoint

--- restapi/routes/register.go
+++ restapi/routes/register.go
@@ -33,3 +33,24 @@ func RegisterForEvent(c *gin.Context) {
 
 	c.JSON(http.StatusCreated, gin.H{"message": "Registered for event successfully"})
 }
+
+func CancelRegistration(c *gin.Context) {
+	userId := c.GetInt64("userId")
+	eventId, err := strconv.ParseInt(c.Param("id"), 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid event ID format"})
+		return
+	}
+
+	var event models.Event
+	event.ID = eventId
+
+	// remove registration from registration table
+	err = event.CancelRegistration(userId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Registration cancelled successfully"})
+}
